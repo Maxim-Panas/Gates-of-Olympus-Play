ec6f16feb01b626047fbe3eb94c15a9625608998
Maxim-Panas <maks.web.all@gmail.com>
2025-02-12 12:04:11 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Показуємо кнопку при прокручуванні вниз
+window.onscroll = function() {
+    let button = document.getElementById('scrollToTop');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      button.classList.add('show');
+    } else {
+      button.classList.remove('show');
+    }
+  };
+  
+  // Прокрутка наверх при натисканні на кнопку
+  document.getElementById('scrollToTop').onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  
